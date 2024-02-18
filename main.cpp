85f657065cf3a06464bb7a50a18975b8cbdf5ee4
carpaiz <32883274+carpaiz@users.noreply.github.com>
2024-02-18 08:33:32 -0600

    agrege el main

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "Estudiante.cpp"
+#include <iostream>
+using namespace std;
+main(){
+	// instanciar el objeto a la clase
+	Estudiante estudiante = Estudiante("Armando","Cardona","Guatemala",444,"2024-02-18","5790");
+	estudiante.leer();
+	
+}
